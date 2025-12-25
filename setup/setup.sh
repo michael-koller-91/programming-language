f7b5df0754f8eed9200a14f000d314d637e7294c
Michael Koller <koller_michael@outlook.de>
2025-12-25 10:21:02 +0100

    move setup files into new folder setup

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir downloads
+mkdir ../downloads
 
 # download and unpack QBE
 wget https://c9x.me/compile/release/qbe-1.2.tar.xz
@@ -11,13 +11,13 @@ cd qbe-1.2
 make
 cd ..
 # copy the binary
-cp qbe-1.2/qbe .
+cp qbe-1.2/qbe ..
 # move to downloads
-mv qbe-1.2 downloads
+mv qbe-1.2 ../downloads
 
 # download and unpack Odin
 wget https://github.com/odin-lang/Odin/releases/download/dev-2025-12a/odin-linux-amd64-dev-2025-12a.tar.gz
 tar -xf odin-linux-amd64-dev-2025-12a.tar.gz
 rm odin-linux-amd64-dev-2025-12a.tar.gz
 # move to downloads
-mv odin-linux-amd64-nightly+2025-12-04 downloads
+mv odin-linux-amd64-nightly+2025-12-04 ../downloads
